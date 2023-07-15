2dc6c76dd7e3e35a19bf0ab181c28b9d02739a4c
evitla <s.dinmuhamed@gmail.com>
2023-07-15 11:57:51 +0600

    feat: grainy bg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,11 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'grainy-indigo-900': 'linear-gradient(to right bottom, rgba(10, 0, 58, 0.81), rgba(9, 0, 48, 0.92)), url("../../public/images/noise.svg")',
+			}
+		},
 	},
 	plugins: [],
 }
