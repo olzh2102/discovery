981eed05ba2e36948efb3e36d42ce1bb3be74a81
olzh2102 <olzhas2102@yahoo.com>
2023-07-23 11:48:27 +0200

    deploy: cloudflare instead vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
-import vercel from '@astrojs/vercel/edge';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
   integrations: [tailwind({
     config: {
       applyBaseStyles: false
