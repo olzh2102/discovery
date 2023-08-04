ff620d92cc48b90eee972302be85148698ba6334
evitla <s.dinmuhamed@gmail.com>
2023-08-04 13:48:59 +0600

    feat: quote component, code tag style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import vercelServerless from '@astrojs/vercel/serverless'
+import mdx from '@astrojs/mdx'
 
 import react from '@astrojs/react'
 
@@ -9,6 +10,7 @@ export default defineConfig({
   output: 'server',
   adapter: vercelServerless(),
   integrations: [
+    mdx(),
     tailwind({
       config: {
         applyBaseStyles: false,
