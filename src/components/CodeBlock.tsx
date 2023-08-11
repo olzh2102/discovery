8b642e0047a68ad63d9c45ec12bd807b447c8c19
evitla <s.dinmuhamed@gmail.com>
2023-08-11 11:12:35 +0600

    feat: copy code button

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,3 +1,8 @@
+import { useState } from 'react'
+
+import checkIcon from '../../public/images/check.svg'
+import copyIcon from '../../public/images/copy.svg'
+
 export default function CodeBlock({
   children,
   path,
@@ -5,13 +10,34 @@ export default function CodeBlock({
   children: React.ReactNode
   path?: string
 }) {
+  const [copied, setCopied] = useState(false)
+
+  async function copyText(text: string) {
+    await navigator.clipboard.writeText(text)
+    setCopied(true)
+    setTimeout(() => setCopied(false), 1000)
+  }
+
   return (
-    <figure className="relative group my-3 text-sm leading-4 bg-[#302749] rounded border border-[#a27afa] overflow-hidden">
+    <figure className="relative group my-3 text-sm leading-4 bg-[#302749] shadow rounded border border-[#43316a]">
       {path && (
-        <figcaption className="text-white">
+        <figcaption className="text-white font-medium">
           <span className="inline-block bg-[#523e7e] px-4 py-2">{path}</span>
         </figcaption>
       )}
+      <button
+        className={`
+          border border-zinc-500 rounded p-1
+          opacity-0 group-hover:opacity-100 hover:bg-[#523e7e]/60 duration-300 transition ease-in-out
+          absolute right-2 ${path ? 'top-10' : 'top-2'}
+        `}
+        onClick={async (e) => {
+          const text = e.currentTarget.nextElementSibling?.querySelector('code')?.innerText
+          if (text) copyText(text)
+        }}
+      >
+        {copied ? <img src={checkIcon} alt="copied" /> : <img src={copyIcon} alt="copy" />}
+      </button>
       {children}
     </figure>
   )
