c4fd19cff737c3c54c5107a01c7f8372a96510ae
evitla <s.dinmuhamed@gmail.com>
2023-08-22 13:37:55 +0600

    feat: article seo

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,7 @@ const post = defineCollection({
   schema: z.object({
     title: z.string(),
     date: z.date(),
-    author: z.string(),
+    authors: z.array(z.string()),
     description: z.string(),
     draft: z.boolean().default(false),
     category: z.enum(['CSS, TailwindCSS', 'JavaScript', 'NextJS', 'ReactJS', 'Test']),
