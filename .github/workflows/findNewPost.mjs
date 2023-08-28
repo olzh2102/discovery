947f3d6b6d1076bd794e6dc467fa47304b0382fc
olzh2102 <olzhas2102@yahoo.com>
2023-08-28 20:36:18 +0200

    test: new post

--- .github/workflows/findNewPost.mjs
+++ .github/workflows/findNewPost.mjs
@@ -1,5 +1,5 @@
 import {$} from 'zx';
 
 console.log(process.env.SHA)
-const modified = await $`git diff --name-only ${process.env.SHA}^1 ${process.env.SHA}`
+const modified = await $`git diff --name-only HEAD^ HEAD`
 console.log("🚀 ~ file: findNewPost.mjs:6 ~ modified:", modified)
