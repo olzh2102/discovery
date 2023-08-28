35dc4c0d6694634f0e67c96fe567b792ec626ffd
evitla <s.dinmuhamed@gmail.com>
2023-08-28 13:34:09 +0600

    feat: gha find post link

--- .github/workflows/notification.mjs
+++ .github/workflows/notification.mjs
@@ -3,12 +3,7 @@ import telegram from 'node-telegram-bot-api'
 
 const bot = new telegram(process.env.TG_TOKEN)
 
-const getTip = () => 'анжуманя'
-
-const main = async () => {
-  const tip = getTip()
-  const message = await bot.sendMessage(process.env.TG_CHAT_ID, tip)
-  console.log(message)
-}
-
-main()
+bot.sendMessage(
+  process.env.TG_CHAT_ID,
+  'process.env.LINK'
+)
