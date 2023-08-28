34c8e1391bf9080f9e3b5e66811c41cec7165356
olzh2102 <olzhas2102@yahoo.com>
2023-08-28 21:18:09 +0200

    cleanup

--- .github/workflows/find-new-post.mjs
+++ .github/workflows/find-new-post.mjs
@@ -1,8 +1,9 @@
 import {$} from 'zx';
 
-console.log(process.env.SHA)
+console.log("🚀 ~ file: find-new-post.mjs:4 ~ process.env.SHA:", process.env.SHA)
 const {stdout: modifiedFiles} = await $`git diff --name-only --diff-filter=AM HEAD^ HEAD`
 const newPostName = modifiedFiles.split('\n').find((file) => file.includes('src/content/post/en'))
+console.log("🚀 ~ file: find-new-post.mjs:6 ~ newPostName:", newPostName)
 
 $`echo link=${JSON.stringify(newPostName)} >> ${process.env.GITHUB_OUTPUT}`;
 
