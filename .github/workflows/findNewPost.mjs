32558808dd8249adb955fcc4fee2c1305e548d75
olzh2102 <olzhas2102@yahoo.com>
2023-08-28 20:47:41 +0200

    test: flag

--- .github/workflows/findNewPost.mjs
+++ .github/workflows/findNewPost.mjs
@@ -1,5 +1,5 @@
 import {$} from 'zx';
 
 console.log(process.env.SHA)
-const modified = await $`git diff --name-only HEAD^ HEAD`
+const modified = await $`git diff --name-only --diff-filter=AM HEAD^ HEAD`
 console.log("🚀 ~ file: findNewPost.mjs:6 ~ modified:", modified)
