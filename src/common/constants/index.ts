d1455e5f7fafb78780c4dd5deca8a9ef89a9bde4
evitla <s.dinmuhamed@gmail.com>
2023-09-01 11:17:12 +0600

    fix: slugMap

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -3,12 +3,12 @@ export const LOCALES = { en: 'en-GB', ru: 'ru-RU' }
 export const WORDS_PER_MINUTE = 180
 
 export const SLUG_MAP = {
-  'localhost-https': {
+  'localhost-https-wild-west': {
     en: '',
     ru: 'localhost-https-soviet',
   },
   'localhost-https-soviet': {
-    en: 'localhost-https',
+    en: 'localhost-https-wild-west',
     ru: '',
   },
 }
