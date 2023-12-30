42dc7f4325465e554338bd4be8e204cfbc3ef4de
olzh2102 <olzhas2102@yahoo.com>
2023-12-30 21:52:26 +0100

    draft: english version

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,23 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection, z } from "astro:content";
 
 const post = defineCollection({
   schema: z.object({
     title: z.string(),
     date: z.date(),
-    authors: z.array(z.string()),
+    authors: z.string().array().nonempty(),
     description: z.string(),
     draft: z.boolean().default(false),
-    category: z.enum(['CSS, TailwindCSS', 'JavaScript', 'NextJS', 'ReactJS', 'Test']),
-    tags: z.array(z.string()),
+    category: z.enum([
+      "CSS, TailwindCSS",
+      "JavaScript",
+      "TypeScript",
+      "NextJS",
+      "ReactJS",
+      "Test",
+    ]),
+    tags: z.string().array().nonempty(),
     heroImageUrl: z.string(),
   }),
-})
+});
 
-export const collections = { post }
+export const collections = { post };
